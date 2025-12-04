d37a47006b58690db94e9daeeb51bebade5f0c7a
Tosman <Tos@man>
2025-12-04 17:45:35 +0800

    imported tailwind in index.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import './App.css'
-
 function App() {
   return (
     <>
-
+      <div className="p-10">
+        <h1 className="text-3xl font-bold text-blue-500">Tailwind is working!</h1>
+        <div className="w-20 h-20 bg-blue-500"></div>
+      </div>
     </>
   )
 }
